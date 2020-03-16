d3ebcf0ffd62dfeabf4b0a21515f6027175cf37e
TammyTee <TammyTee@users.noreply.github.com>
2020-03-16 09:55:55 -0400

    - apply custom tailwind styles inside extend object in the config - module index progress

--- resources/views/modules/index.blade.php
+++ resources/views/modules/index.blade.php
@@ -2,18 +2,48 @@
 
 @section('content')
 <div class="w-full bg-white">
-    <div class="text-center px-6 py-12 bg-gray-100 border-b">
-        <h1 class="text-xl md:text-4xl pb-4">{{ __('Modules') }}</h1>
-        <p class="leading-loose text-gray-dark">
-            {{ __('The tech concepts you should know in order to get a job as a Laravel developer.') }}
-        </p>
+    <div class="bg-indigo-100 pt-16 pb-16">
+        <div class="container mx-auto px-5 lg:px-12">
+            <h1>{{ __('Modules') }}</h1>
+            <p class="leading-normal mt-4">
+                {{ __('The tech concepts you should know in order to get a job as a Laravel developer.') }}
+            </p>
+        </div>
     </div>
 
     @include('partials.you-should-log-in')
 
-    <div class="container max-w-4xl mx-auto md:flex items-start mt-6 py-8 px-12 md:px-0">
-        <div class="w-full md:pr-12 mb-6">
-            <p class="mb-2 font-bold text-lg">Recommended modules</p>
+    <div class="container mx-auto pt-12">
+        {{-- <div class="w-full px-5"> --}}
+            <p class="font-semibold leading-tight text-xl mb-10">All modules</p>
+
+            <div class="flex flex-wrap w-full px-2">
+                @foreach ($standardModules as $module)
+                    <div class="px-2 w-full sm:w-1/2 lg:w-1/3 xl:w-1/4">
+                        <a class="mb-5 bg-white shadow-md block" href="{{ route_wlocale('modules.show', $module) }}">
+                            <span class="bg-teal-600 pb-8/12 block"></span>
+                            <span class="p-5 pb-6 block">
+                                <p class="font-semibold">{{ $module->name }}</p>
+                            </span>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+
+            @if ($bonusModules->isNotEmpty())
+            <p class="font-semibold leading-tight text-xl mb-10">Bonus modules</p>
+
+            @foreach ($bonusModules as $module)
+                <a class="mb-5 bg-white shadow-md block" href="{{ route_wlocale('modules.show', $module) }}">
+                    <span class="bg-teal-600 pb-8/12 block"></span>
+                    <span class="p-5 pb-6 block">
+                        <p class="font-semibold">{{ $module->name }}</p>
+                    </span>
+                </a>
+            @endforeach
+            @endif
+
+            {{-- <p class="mb-2 font-bold text-lg">Recommended modules</p>
 
             <ul class="@guest list-disc @endguest">
                 @foreach ($standardModules as $module)
@@ -47,8 +77,8 @@
                 </li>
                 @endforeach
             </ul>
-            @endif
-        </div>
+            @endif --}}
+        {{-- </div> --}}
     </div>
 </div>
 @endsection
