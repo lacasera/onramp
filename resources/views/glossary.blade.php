6b5477933bdc0857786b36fed668e04564572604
TammyTee <TammyTee@users.noreply.github.com>
2020-03-20 14:27:05 -0400

    - custom utilities and components for reusability - refactor nav, home, and module index

--- resources/views/glossary.blade.php
+++ resources/views/glossary.blade.php
@@ -2,53 +2,66 @@
 
 @section('content')
 <div class="w-full bg-white">
-    <div class="text-center px-6 py-12 bg-gray-100 border-b">
-        <h1 class=" text-xl md:text-4xl">{{ __('Glossary') }}</h1>
+    <div class="bg-indigo-100 overflow-hidden py-16 lg:py-24">
+        <div class="fluid-container relative">
+            <h1 class="max-w-lg">{{ __('Glossary') }}</h1>
+
+            <picture>
+                <source media="(min-width: 1024px)"
+                    srcset="/images/shapes/double-curve-dark-large.svg">
+
+                <img
+                    class="absolute h-670-px -mr-32 opacity-10 pointer-events-none right-0 top-1/2 transform -translate-y-1/2 lg:h-1340-px lg:-mr-48 lg:opacity-100"
+                    src="/images/shapes/single-curve-dark-small.svg"
+                    alt="Onramp">
+            </picture>
+        </div>
     </div>
 
-    <div class="container max-w-4xl mx-auto md:flex items-start py-8 px-6 md:px-0">
+    <div class="fluid-container md:flex items-start py-8">
         <div class="w-full md:pr-12 mb-6">
             <div class="flex flex-col-reverse md:flex-row">
                 <ul class="md:w-3/4 flex-grow pr-8">
                     @forelse ($terms as $term)
-                    <li id="{{ $term->getEnglishName() }}" class=" md:mt-6 p-4 list-none">
-                        <div class="flex justify-start items-baseline mb-1 md:mb-3">
-                            <a class="text-lg font-bold" href="#{{ $term->getEnglishName() }}">#</a>
-                            <h3 class="ml-2 font-bold text-lg capitalize">
-                                {{ $term->getTranslation('name', locale()) }}
-                            </h3>
-                            @if ($term->name !== $term->getEnglishName())
-                                <span class="pl-1 text-grey-800 capitalize text-sm">({{ $term->getEnglishName() }})</span>
-                            @endif
-                        </div>
-                        <p class="mt-2">{{ $term->getTranslation('description', locale()) }}</p>
-                        @if ($term->resourcesForCurrentSession()->count() > 0)
-                        <div class="mt-4 flex flex-col">
-                            <span class="text-gray-800">Related resources:</span>
-                            @foreach ($term->resourcesForCurrentSession()->get() as $resource)
-                                <span>
-                                    <a href="{{ route_wlocale('modules.show', $resource->module()->first()) }}">{{ $resource->module()->first()->name }}</a>
-                                    &gt;
-                                    <a href="{{ $resource->url }}">{{ $resource->name }} <img src="/images/outbound_link_icon.svg" alt="Outbound link" class="w-4 inline align-text-top"></a>
-                                </span>
-                            @endforeach
-                        </div>
-                        @endif
-                        @if ($term->relatedTerms->count() > 0)
-                            <div class="mt-4 flex items-center">
-                                <span class="text-gray-800">Related Terms:</span>
-                                @foreach ($term->relatedTerms as $relatedTerm)
-                                    <a class="ml-2 px-3 bg-gray-400 rounded-full text-sm font-semibold text-gray-700" href="#{{ $relatedTerm->name }}">#{{ $relatedTerm->name }}</a>
+                        <li id="{{ $term->getEnglishName() }}" class=" md:mt-6 py-4 list-none">
+                            <div class="flex justify-start items-baseline mb-1 md:mb-3">
+                                <a class="text-lg font-bold" href="#{{ $term->getEnglishName() }}">#</a>
+                                <h3 class="ml-2 font-bold text-lg capitalize">
+                                    {{ $term->getTranslation('name', locale()) }}
+                                </h3>
+                                @if ($term->name !== $term->getEnglishName())
+                                    <span class="pl-1 text-grey-800 capitalize text-sm">({{ $term->getEnglishName() }})</span>
+                                @endif
+                            </div>
+                            <p class="mt-2">{{ $term->getTranslation('description', locale()) }}</p>
+                            @if ($term->resourcesForCurrentSession()->count() > 0)
+                            <div class="mt-4 flex flex-col">
+                                <span class="text-gray-800">Related resources:</span>
+                                @foreach ($term->resourcesForCurrentSession()->get() as $resource)
+                                    <span>
+                                        <a href="{{ route_wlocale('modules.show', $resource->module()->first()) }}">{{ $resource->module()->first()->name }}</a>
+                                        &gt;
+                                        <a href="{{ $resource->url }}">{{ $resource->name }} <img src="/images/outbound_link_icon.svg" alt="Outbound link" class="w-4 inline align-text-top"></a>
+                                    </span>
                                 @endforeach
                             </div>
-                        @endif
-                    </li>
+                            @endif
+                            @if ($term->relatedTerms->count() > 0)
+                                <div class="mt-4 flex items-center">
+                                    <span class="text-gray-800">Related Terms:</span>
+                                    @foreach ($term->relatedTerms as $relatedTerm)
+                                        <a class="ml-2 px-3 bg-gray-400 rounded-full text-sm font-semibold text-gray-700" href="#{{ $relatedTerm->name }}">#{{ $relatedTerm->name }}</a>
+                                    @endforeach
+                                </div>
+                            @endif
+                        </li>
                     @empty
-                    <li class="m-4 mt-6 list-none">
-                        {{ __('No terms') }}
-                    </li>
+                        <li class="m-4 mt-6 list-none">
+                            {{ __('No terms') }}
+                        </li>
                     @endforelse
                 </ul>
+
                 <div class="md:mt-10 mb-4 pb-4 w-full md:w-1/4 border-b border-grey-100 md:border-none">
                     <h3 class="text-xl">{{ __('Table of contents') }}</h3>
                     <toggle-when-mobile class="mb-2">
